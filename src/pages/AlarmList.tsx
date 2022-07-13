040c9f7a3bfec1c2bfcae6574cc1c4b3006e4850
seolyeongbae <peixueying@gmail.com>
2022-07-12 21:58:22 -0700

    feat : set alarmList, alarmSetting, alarmRing page

---
+++ src/pages/AlarmList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function AlarmList() {
+  return (
+    <View>
+      <Text>알람 리스트</Text>
+    </View>
+  );
+}
+
+export default AlarmList;
--- src/pages/Alarms.tsx
+++ src/pages/Alarms.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableHighlight} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import AlarmList from './AlarmList';
+import AlarmRing from './AlarmRing';
+import AlarmSettings from './AlarmSettings';
+
+const Stack = createNativeStackNavigator();
 
 function Alarms() {
   return (
-    <View>
-      <Text>알람 목록</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Alarms"
+        component={AlarmList}
+        options={params => ({
+          title: 'Alarms',
+          headerRight: () => (
+            <AddButton
+              title={'+ '}
+              onPress={() => params.navigation.navigate('Edit')}
+            />
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="Edit"
+        component={AlarmSettings}
+        options={{title: 'Alarm'}}
+      />
+      <Stack.Screen
+        name="Ring"
+        component={AlarmRing}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
 }
+
+function AddButton({title, onPress}: any) {
+  return (
+    <TouchableHighlight
+      style={styles.button}
+      onPress={onPress}
+      underlayColor="#fff">
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'transparent',
+    padding: 10,
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+});
 
 export default Alarms;
