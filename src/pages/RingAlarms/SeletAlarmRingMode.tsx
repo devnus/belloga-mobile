de5e686b5745f31c576d54dc507f30410626e02c
seolyeongbae <peixueying@gmail.com>
2022-10-31 23:52:14 +0900

    feat: handling labeling alert and common alert for network condition

--- src/pages/RingAlarms/SeletAlarmRingMode.tsx
+++ src/pages/RingAlarms/SeletAlarmRingMode.tsx
@@ -3,9 +3,21 @@ import Alarm, {getAlarm} from '@modules/alarms';
 import LabelingAlarmRing from './LabelingAlarmRing';
 import CommonAlarmRing from './CommonAlarmRing';
 import {Text} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 
 function SelectAlarmRingMode({route, navigation}) {
   const [alarm, setAlarm] = useState<Alarm | undefined>();
+  const [connectionInfo, setNetInfo] = useState<boolean | null>(false);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setNetInfo(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   useEffect(() => {
     const alarmUid = route.params.alarmUid;
@@ -16,13 +28,15 @@ function SelectAlarmRingMode({route, navigation}) {
     })();
   }, []);
 
+  useEffect(() => {});
+
   if (!alarm) {
     return <Text> 로딩중입니다!</Text>;
   }
 
   return (
     <>
-      {alarm?.isMissionAlert === true ? (
+      {alarm?.isMissionAlert === true && connectionInfo === true ? (
         <LabelingAlarmRing
           route={route}
           navigation={navigation}
